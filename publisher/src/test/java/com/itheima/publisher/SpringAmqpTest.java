d4c55f982ed41f69c7d0fde69ac198578765b372
linzhuofei <philip.f.lin@gmail.com>
2025-06-29 23:04:07 +0800

    MQ入門 - Java 客戶端 - 快速入門

--- publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
+++ publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -1,7 +1,27 @@
 package com.itheima.publisher;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class SpringAmqpTest {
 
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
+    @Test
+    public void testSimpleQueue(){
+        // 1. 隊列名
+        String queueName = "simple.queue";
+
+        // 2. 消息
+        String message = "hello, spring amqp!";
+
+        // 3. 發送消息
+        rabbitTemplate.convertAndSend(queueName, message);
+    }
 }
