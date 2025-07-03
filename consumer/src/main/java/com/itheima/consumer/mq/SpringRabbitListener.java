387267c9a374c054e76853be72e93165b76702f3
linzhuofei <philip.f.lin@gmail.com>
2025-07-03 23:18:55 +0800

    MQ入門 - Java 客戶端 - WorkQueue

--- consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
+++ consumer/src/main/java/com/itheima/consumer/mq/SpringRabbitListener.java
@@ -4,6 +4,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalTime;
+
 @Slf4j
 @Component
 public class SpringRabbitListener {
@@ -11,5 +13,15 @@ public class SpringRabbitListener {
     @RabbitListener(queues = "simple.queue")
     public void listenSimpleQueue(String message){
         log.info("監聽到 simple.queue 的消息: {}", message);
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenSimpleQueue1(String message){
+        log.info("消費者 1 接收到消息: " + message + ", " + LocalTime.now());
+    }
+
+    @RabbitListener(queues = "work.queue")
+    public void listenSimpleQueue2(String message){
+        log.info("消費者 2 ....... 接收到消息: " + message + ", " + LocalTime.now());
     }
 }
