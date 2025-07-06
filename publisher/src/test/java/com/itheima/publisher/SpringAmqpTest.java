c06007995a043b02089e12e3d49a79bd357a6c69
linzhuofei <philip.f.lin@gmail.com>
2025-07-06 13:15:21 +0800

    MQ入門 - Java 客戶端 - WorkQueue

--- publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
+++ publisher/src/test/java/com/itheima/publisher/SpringAmqpTest.java
@@ -25,4 +25,18 @@ class SpringAmqpTest {
         rabbitTemplate.convertAndSend(queueName, message);
 
     }
+
+    @Test
+    public void testWorkQueue(){
+        // 1. 隊列名
+        String queueName = "work.queue";
+
+        for (int i = 1; i <= 50 ; i++) {
+            // 2. 消息
+            String message = "hello, spring amqp_" + i ;
+
+            // 3. 發送消息
+            rabbitTemplate.convertAndSend(queueName, message);
+        }
+    }
 }
